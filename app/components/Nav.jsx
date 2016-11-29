9d0c7ac0de8b9970ac36ba20c23957cfcbb1ae9b
Sam Park <sampark18@gmail.com>
2016-11-29 14:17:59 -0800

    Add active class names

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,13 +1,13 @@
 var React = require('react');
-var {Link} = require('react-router');
+var {Link, IndexLink} = require('react-router');
 
 var Nav = React.createClass({
   render: function () {
     return (
       <div>
         <h2>Nav Component</h2>
-          <Link to='/'>Get the Weather</Link>
-          <Link to='about'>About</Link>
+          <IndexLink to='/' activeClassName="active">Get the Weather</IndexLink>
+          <Link to='about' activeClassName="active">About</Link>
       </div>
     );
   }
